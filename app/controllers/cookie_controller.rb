8661303a0813999a54b2ba81d9cbf2a3630d0f2f
Luciano Lopes <lucianos@algartelecom.com.br>
2016-06-06 18:18:36 -0300

    jeader

--- app/controllers/cookie_controller.rb
+++ app/controllers/cookie_controller.rb
@@ -1,7 +1,8 @@
 class CookieController < ApplicationController
   def latest
     screen = Screen::Overview.new
-    @response.headers["Access-Control-Allow-Origin"] = "*"
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
     render json: Cookie.latest
   end
 end

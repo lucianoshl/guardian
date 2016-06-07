7dbde94f8f9d4bfb6fae61158512f99285304d37
Luciano Lopes <lucianos@algartelecom.com.br>
2016-06-07 11:42:25 -0300

    ahhh

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,17 +7,15 @@ before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
+    spawn("bundle exec bin/delayed_job stop") 
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  spawn("bundle exec bin/delayed_job start") 
+
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end

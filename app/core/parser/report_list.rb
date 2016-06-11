dacbf66345e7a13460d9b3360505aa2589af8997
Luciano Lopes <lucianos@algartelecom.com.br>
2016-06-11 13:35:59 -0300

    fixes

--- app/core/parser/report_list.rb
+++ app/core/parser/report_list.rb
@@ -2,6 +2,7 @@ class Parser::ReportList < Parser::Basic
 
   def parse(screen)
   	super
+    binding.pry
     screen.report_ids = @page.search('#report_list .quickedit').map{|a| a.attr('data-id').to_i}
   end
 

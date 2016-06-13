8e7cf8d1245444a941e00e46fd56087f187cdf06
Luciano Lopes <lucianoshl@gmail.com>
2016-06-13 03:08:11 -0300

    vai que dá

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,18 +4,18 @@ timeout 15
 preload_app true
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    spawn("bin/delayed_job stop") 
-    Process.kill 'QUIT', Process.pid
-  end 
+  # Signal.trap 'TERM' do
+  #   puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+  #   spawn("bin/delayed_job stop") 
+  #   Process.kill 'QUIT', Process.pid
+  # end 
 
-  $pid = spawn("bin/delayed_job -n 1 --log-dir=#{Rails.root}/log start ") 
+  # $pid = spawn("bin/delayed_job -n 1 --log-dir=#{Rails.root}/log start ") 
 
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
+  # Signal.trap 'TERM' do
+  #   puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  # end
 end

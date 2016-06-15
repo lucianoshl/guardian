10e07eb51b4c9e3c59d17d0e14dee6b4d20dc3aa
Luciano Lopes <lucianoshl@gmail.com>
2016-06-15 08:48:05 -0300

    front-end

--- app/helpers/resource_helper.rb
+++ app/helpers/resource_helper.rb
@@ -1,5 +1,6 @@
 module ResourceHelper
   def render_resource resource
+    return "" if (resource.nil?)
     %{
       <img src="https://help.tribalwars.com.br/images/2/20/Made.png"> #{resource.wood}
       <img src="https://help.tribalwars.com.br/images/8/8e/Argila.png"> #{resource.stone}
---
+++ spec/core/screen/simulator_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Screen::Simulator do
+  it "do_login" do
+
+    
+    
+    Screen::Simulator.offline()
+  end
+end

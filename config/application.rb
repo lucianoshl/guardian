09bfc5c9e70442acf518323500cbefe220e14215
Luciano Lopes <lucianoshl@gmail.com>
2016-06-15 23:24:46 -0300

    fix log

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module Guardian
     config.time_zone = Time.zone = 'Brasilia'
 
     Mongoid.logger.level = Logger::INFO
-    Moped.logger.level = Logger::INFO
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

37c8f9432f1465b8b28f77c0bfd53fadcfa4446e
Luciano Lopes <lucianos@algartelecom.com.br>
2016-06-18 16:04:18 -0300

    fix unit tests

---
+++ spec/core/screen/screen_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Screen::ReportView, type: :model do
+
+  before(:all) do
+    @params = {vilage: Village.my.first.vid}
+  end
+
+  it "Main" do
+    Screen::Main.new( @params )
+  end
+
+  it "Place" do
+    Screen::Place.new( @params )
+  end
+
+  it "ReportList" do
+    Screen::ReportList.new(mode: 'attack')
+  end
+
+  it "ReportView" do
+    Screen::ReportView.new(view: 16750852)
+  end
+
+  it "Simulator" do
+    Screen::Simulator.new( @params )
+  end
+
+  it "StatsOwn" do
+    Screen::StatsOwn.new( @params )
+  end
+
+end

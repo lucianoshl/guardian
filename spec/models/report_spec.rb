b8e6a89b8bdbe34ce5299b1c49cdd3fedb1216be
Luciano Lopes <lucianoshl@gmail.com>
2016-07-08 00:04:23 -0300

    fixes

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Report, type: :model do
   it "rams_to_destroy_wall" do
     report = Report.gt("target_buildings.wall" => 0 ).first
-    report.rams_to_destroy_wall
+    if (!report.nil?)
+      report.rams_to_destroy_wall
+    end
   end
 
   it "pillage_statistics" do

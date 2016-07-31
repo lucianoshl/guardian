1eed68790b19c8ba160c15ca3f294d5d1c413349
Luciano Lopes <lucianoshl@gmail.com>
2016-07-31 16:36:42 -0300

    fix behavior

--- app/assets/javascripts/village.js
+++ app/assets/javascripts/village.js
@@ -1,3 +1,4 @@
 $(document).on('click', 'tr.village-line', function(){
-  window.location.href = '/village/' + $(this).attr('village-id');
+  // window.location.href = '/village/' + $(this).attr('village-id');
+  window.open('/village/' + $(this).attr('village-id'), '_blank');
 });

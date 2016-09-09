0f3c38b4bc21ad5c9bae7d8097b3c504cb847cc4
Luciano Lopes <lucianoshl@gmail.com>
2016-09-08 23:34:15 -0300

    remove bp

--- spec/core/mobileeg.rb
+++ spec/core/mobileeg.rb
@@ -2,20 +2,6 @@ require 'rails_helper'
 
 RSpec.describe do
 	it "mobileeg" do  
-
-
-
-		
-
-		# token = login_result['result']['token']
-
-
-
-		# uri = "https://www.tribalwars.com.br/m/m/worlds"
-		# worlds_result = JSON.parse(client.post(uri,[token]).body)
-
-
-		binding.pry
 	end
 end
 

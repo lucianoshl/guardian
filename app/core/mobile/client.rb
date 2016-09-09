237d1b0fae14acd08e7c9fa37fe4e25443466faa
Luciano Lopes <lucianoshl@gmail.com>
2016-09-08 23:44:57 -0300

    ai si

--- app/core/mobile/client.rb
+++ app/core/mobile/client.rb
@@ -4,8 +4,8 @@ class Mobile::Client
 
     @@global.user_agent = "Mozilla/5.0 (Linux; Android 4.4.4; SAMSUNG-SM-N900A Build/tt) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36"
 
-    @@global.set_proxy("localhost", 8081)
-    @@global.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # @@global.set_proxy("localhost", 8081)
+    # @@global.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     @@global.add_cookies(MobileCookie.latest)
 

54f0c9a8c002f8c528aee944b1f25a77b9beb2f9
Luciano Lopes <lucianoshl@gmail.com>
2016-09-18 18:13:12 -0300

    validatiosn

---
+++ config/initializers/0_validate_env.rb
@@ -0,0 +1,10 @@
+variables = ['MONGO_URL','HEROKU_KEY','TW_USER','TW_PASSWORD','TW_WORLD']
+invalid_config = variables.select{ |a| !ENV[a].nil? }.empty?
+if (invalid_config)
+	msg = 'As seguintes variaveis estão incompletas:'
+	variables.map do |variable|
+		msg += "\n #{variable}=#{ENV[variable]}"
+	end
+	Rails.logger.error(msg)
+	raise Exception.new(msg)
+end

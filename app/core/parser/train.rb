73d7ce9c82c896ea1291d6c2eb918c00ca72ef9a
Luciano Lopes <lucianoshl@gmail.com>
2016-09-27 21:45:54 -0300

    fix bug

--- app/core/parser/train.rb
+++ app/core/parser/train.rb
@@ -7,7 +7,7 @@ class Parser::Train < Parser::Basic
     screen.build_info = {}
     @page.search('#train_form > .mobileBlock').map do |element|
       unit = element.search('input').attr('name').value
-      current,total = element.search('.imageContainer').text.strip.split('/').map(&:to_i
+      current,total = element.search('.imageContainer').text.strip.split('/').map(&:to_i)
       screen.current_units[unit] = current
       screen.all_units[unit] = total
     end

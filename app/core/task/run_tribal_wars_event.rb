2667da20dca51ffe82aba766045afb55c65a4b61
Luciano Lopes <lucianoshl@gmail.com>
2016-11-06 16:43:30 -0200

    fix treta

--- app/core/task/run_tribal_wars_event.rb
+++ app/core/task/run_tribal_wars_event.rb
@@ -1,20 +1,21 @@
 class Task::RunTribalWarsEvent < Task::Abstract
 
   def run
-    screen = Screen::EventAssault.new
-    Rails.logger.info("Event enable: #{screen.enabled}".white.on_blue)
-    if (!screen.enabled)
-        return Time.zone.now + 1.month
-    end
-    Rails.logger.info("We have #{screen.mercenaries_amount} free in event")
-    if (screen.mercenaries_amount > 0)
-    	event = screen.best_event
-    	Rails.logger.info("Best target for event is #{event}")
-    	screen.put_mercenary(event)
-    	return Time.zone.now
-    else
-        return screen.next_free_mercenary
-    end
+    # screen = Screen::EventAssault.new
+    # Rails.logger.info("Event enable: #{screen.enabled}".white.on_blue)
+    # if (!screen.enabled)
+    #     return Time.zone.now + 1.month
+    # end
+    # Rails.logger.info("We have #{screen.mercenaries_amount} free in event")
+    # if (screen.mercenaries_amount > 0)
+    # 	event = screen.best_event
+    # 	Rails.logger.info("Best target for event is #{event}")
+    # 	screen.put_mercenary(event)
+    # 	return Time.zone.now
+    # else
+    #     return screen.next_free_mercenary
+    # end
+    return Time.zone.now + 1.day
   end
 
 end

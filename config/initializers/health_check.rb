a52475b3f866396d6ab2ca95ff2b17cd00cf644a
Luciano Lopes <lucianoshl@gmail.com>
2016-12-18 13:24:43 -0200

    fix logic

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -1,7 +1,16 @@
 HealthCheck.setup do |config|
 
   config.add_custom_check do
-    Delayed::Job.where(:last_error.nin => [nil]).count == 0 ? "" : nil
+    job_without_error = Delayed::Job.where(:last_error.nin => [nil]).count == 0
+
+
+    locked_jobs = Delayed::Job.where(:locked_by.nin => [nil]).to_a
+
+    locked_delayed = locked_jobs.select{|a| (Time.zone.now - a.locked_at) > 20.minutes }
+
+    locked_delayed.map {|a| a.locked_at = nil; a.locked_by = nil; a.save }
+
+    job_without_error ? "" : nil
   end
 
 end

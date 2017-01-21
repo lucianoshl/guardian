0eccab595d0ad8ab139d9981b187f9eb5f177cc6
Luciano Lopes <lucianoshl@gmail.com>
2017-01-21 13:27:04 -0200

    fixes

--- app/helpers/global_helper.rb
+++ app/helpers/global_helper.rb
@@ -1,6 +1,6 @@
 module GlobalHelper
   def generate_url args
-  	if (!args.include?('village'))
+  	if (!args.keys.include?(:village))
   		return "/game.php?village=#{@vid}&" + args.to_query
   	else
   		return "/game.php?" + args.to_query

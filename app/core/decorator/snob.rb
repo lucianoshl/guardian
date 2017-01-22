e03b0ee0f0c62e32730030964efe5a29ae6ca4ac
Luciano Lopes <lucianoshl@gmail.com>
2017-01-22 15:59:06 -0200

    test

--- app/core/decorator/snob.rb
+++ app/core/decorator/snob.rb
@@ -1,18 +1,18 @@
 class Decorator::Snob
 
 	def html(page)
-		coin_cost =  page.search('.mint_coins_req').text.strip.split("\n").map(&:extract_number).to_resource
-		current = page.search('.header-border.menu_block_right').first.text.split.to_resource
+		# coin_cost =  page.search('.mint_coins_req').text.strip.split("\n").map(&:extract_number).to_resource
+		# current = page.search('.header-border.menu_block_right').first.text.split.to_resource
 
-		missing = coin_cost - current
+		# missing = coin_cost - current
 
-		if (missing.has_positive?)
-			element = page.search('.mint_coins_req').first.parents(2).search('.inactive').first
-			element.content = ""
-			element.add_next_sibling(missing.to_html)
-			table_title = page.search('.mint_coins_req').first.parents(3).search('th').last
-			table_title.content = "Falta"
-		end
+		# if (missing.has_positive?)
+		# 	element = page.search('.mint_coins_req').first.parents(2).search('.inactive').first
+		# 	element.content = ""
+		# 	element.add_next_sibling(missing.to_html)
+		# 	table_title = page.search('.mint_coins_req').first.parents(3).search('th').last
+		# 	table_title.content = "Falta"
+		# end
 
 		return page
 	end

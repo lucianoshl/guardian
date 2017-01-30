9c2ec5983a5ced5f4f193de27fc7ab46ca92c9bf
Luciano Lopes <lucianoshl@gmail.com>
2017-01-29 23:58:15 -0200

    fix

--- app/controllers/tribal_wars_controller.rb
+++ app/controllers/tribal_wars_controller.rb
@@ -43,7 +43,7 @@ class TribalWarsController < ApplicationController
       page = client.send(method,uri,headers)
     end
 
-    Rails.logger.info("ACAO INVALIDA".on_red) if (page.body.include?("Ação inválida"))
+    Rails.logger.info("ACAO INVALIDA".on_red) if (page.body.scan(/A..o inv.lida/).size > 0)
     
 
     if page.uri.to_s.include?('map.php')

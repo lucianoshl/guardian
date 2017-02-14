842afe9d4d9583c3241bfec4959d6d125df45e28
Luciano Lopes <lucianoshl@gmail.com>
2017-02-14 03:34:29 -0200

    partial

--- app/models/job/snob_target.rb
+++ app/models/job/snob_target.rb
@@ -6,17 +6,52 @@ class Job::SnobTarget < Job::Abstract
 	field :y, type: Integer
 
 	def execute
-		target = Village.where(x:x,y:y).first
+		# target = Village.where(x:x,y:y).first
 
-		troops_to_destroy = target.last_report.target_troops
+		# troops_to_destroy = target.last_report.target_troops
 
-		atks_village = Model::Village.where(name: 'ATAQUE').first.villages.map(&:vid)
+		# atks_village = Model::Village.where(name: 'ATAQUE').first.villages.map(&:vid)
 
-		my_troops = atks_village.map { |vid| Screen::Place.new(village: vid) }
+		# my_troops = atks_village.map { |vid| Screen::Place.new(village: vid) }
 
-		fulls = my_troops.select { |p| u = p.units; u.population > 18000 && u.axe > 4000 && u.light > 2000 }
+		# fulls = my_troops.select { |p| u = p.units; u.population > 18000 && u.axe > 4000 && u.light > 2000 }
 
-		binding.pry
+
+		# event = Time.now.at_beginning_of_day + 8.hours + 10.minutes
+		# atks = fulls.map do |place|
+		# 	item = Job::SendAttack.new()
+		# 	item.origin = "#{place.village.x}|#{place.village.y}"
+		# 	item.troop = Troop.new
+		# 	item.troop.axe = -1 if (place.units.axe > 0)
+		# 	item.troop.light = -1 if (place.units.axe > 0)
+		# 	item.troop.ram = -1 if (place.units.ram > 0)
+		# 	item.troop.spy = 5 if (place.units.axe > 5)
+
+		# 	aux = item.troop.clone
+		# 	aux.my_fields.map do |field|
+		# 		aux[field] = 1 if (aux[field] != 0)
+		# 	end
+
+		# 	travel_time = aux.travel_time(place.village,target)
+		# 	arrival_time = Time.zone.now + travel_time + 2.minutes
+
+		# 	if (travel_time > 5.hours)
+		# 		raise Exception.new
+
+		# 	if ( arrival_time > event)
+		# 		item.event_time = arrival_time
+		# 	else 
+		# 		item.event_time = event
+		# 	end
+
+		# 	item.coordinate = '406|423'
+		# 	item
+		# end
+
+		# binding.pry
+
+
+		# binding.pry
 	end
 
 end
--- spec/core/task/auto_recruit_spec.rb
+++ spec/core/task/auto_recruit_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe Task::AutoRecruit, type: :model do
   end
 
   it "do_task" do 
-  	Job::SnobTarget.new(x: 406, y: 423).execute
+    Job::SendAttack.find('58a2949fe6335c0a3789b6e9').execute
+
   	
   end
 

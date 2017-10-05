05883fce7424fab91e6ac082277c51043d2c9a0d
Luciano Lopes <lucianoshl@gmail.com>
2017-10-05 00:56:54 -0300

    automatize

--- config/initializers/tasks.rb
+++ config/initializers/tasks.rb
@@ -1,16 +1,16 @@
-# Dir["#{Rails.root}/app/core/task/*.rb"].map{|f| ActiveSupport::Dependencies.load_file f }
+Dir["#{Rails.root}/app/core/task/*.rb"].map{|f| ActiveSupport::Dependencies.load_file f }
 
-# Task.constants.map do |const|
-#   task = Task.const_get(const)
-#   Rails.logger.info("Running init_schedules for #{task}")
+Task.constants.map do |const|
+  task = Task.const_get(const)
+  Rails.logger.info("Running init_schedules for #{task}")
+  task.init_schedules if (task.respond_to?('init_schedules'))
+end
+
+
+# Dir["#{Rails.root}/app/models/job/*.rb"].map{|f| ActiveSupport::Dependencies.load_file f }
+
+# Job.constants.map do |const|
+#   task = Job.const_get(const)
 #   task.init_schedules if (task.respond_to?('init_schedules'))
 # end
-
-
-# # Dir["#{Rails.root}/app/models/job/*.rb"].map{|f| ActiveSupport::Dependencies.load_file f }
-
-# # Job.constants.map do |const|
-# #   task = Job.const_get(const)
-# #   task.init_schedules if (task.respond_to?('init_schedules'))
-# # end
-# #  
+#  

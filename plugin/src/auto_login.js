4e1c8bf3b56b954ff6e4872c2271f38f845fac8b
Luciano Lopes <lucianoshl@gmail.com>
2017-11-07 18:02:34 -0200

    clean files

--- plugin/src/auto_login.js
+++
@@ -1,6 +0,0 @@
-document.write("getting remote session");
-$.getJSON("https://guardianl.herokuapp.com/cookie/latest", function(session){
-	extension.redirect_to_session(session, function(){
-		window.location.href = session.redirected_page
-	});
-});

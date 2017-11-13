a19116f276e71a6e2f7e067b383e27a143bbac19
Luciano Lopes <lucianoshl@gmail.com>
2017-11-13 21:45:27 -0200

    nice\!\!\!

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,12 +3,12 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  field :village, Types::VillageType do
+  field :village, types[Types::VillageType] do
     description "An example field added by the generator"
-    # argument :id, !types.String
-    argument :name, !types.String
+    argument :id, !types.String
+    # argument :name, !types.String
     resolve ->(obj, args, ctx) {
-      Village.where(args.to_h).first
+      Village.where(args.to_h)
     }
   end
 
